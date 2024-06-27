2176a4d2d602fd45bfdcfd77483838962a1b0c69
bernadinedao <bernadine.dao@gmail.com>
2024-06-28 06:16:10 +1000

    update timeout

--- routes/spoonacular_router.js
+++ routes/spoonacular_router.js
@@ -17,6 +17,7 @@ router.get("/spoonacularApi/recipes", async (req, res) => {
           limitLicense: true,
           ignorePantry: false,
         },
+        timeout: 30000,
       }
     );
     console.log("Spoonacular API Response:", response.data);
@@ -31,11 +32,22 @@ router.get("/spoonacularApi/recipeIngredients", async (req, res) => {
   const { id } = req.query;
   try {
     const response = await axios.get(
-      `https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=${process.env.SPOONACULAR_KEY}`
+      `https://api.spoonacular.com/recipes/${id}/ingredientWidget.json`,
+      {
+        params: {
+          apiKey: process.env.SPOONACULAR_KEY,
+        },
+        timeout: 30000,
+      }
     );
     res.json(response.data);
   } catch (err) {
-    res.status(500).send(err.message);
+    console.error(
+      "Error fetching recipe ingredients from Spoonacular:",
+      err.message
+    );
+    console.error("Error details:", err.response ? err.response.data : err);
+    res.status(500).send(`Error: ${err.message}`);
   }
 });
 
@@ -43,7 +55,13 @@ router.get("/spoonacularApi/recipeInstructions", async (req, res) => {
   const { id } = req.query;
   try {
     const response = await axios.get(
-      `https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=${process.env.SPOONACULAR_KEY}`
+      `https://api.spoonacular.com/recipes/${id}/analyzedInstructions`,
+      {
+        params: {
+          apiKey: process.env.SPOONACULAR_KEY,
+        },
+        timeout: 30000,
+      }
     );
     res.json(response.data);
   } catch (err) {
