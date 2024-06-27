35f67f3402aed7cd167807fe9db8a39d62bcba6d
bernadinedao <bernadine.dao@gmail.com>
2024-06-27 17:10:55 +1000

    about to change to lambda

---
+++ routes/auth_router.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.Router();
+const session = require("express-session");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+router.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+router.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.OAUTH_CLIENT_ID,
+      clientSecret: process.env.OAUTH_SECRET,
+      callbackURL: "http://localhost:8080/auth/google/callback",
+      scope: ["profile", "email"],
+    },
+    function (accessToken, refreshToken, profile, done) {
+      const user = {
+        id: profile.id,
+        displayName: profile.displayName,
+        email: profile.emails[0].value,
+      };
+      done(null, user);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    const token = jwt.sign(
+      { id: req.user.id, name: req.user.displayName, email: req.user.email },
+      process.env.SESSION_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.redirect(`http://localhost:5173/?token=${token}`);
+  }
+);
+
+router.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("http://localhost:5173");
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,92 +1,15 @@
 require("dotenv").config();
-
 const express = require("express");
 const app = express();
 const port = 8080;
-const session = require("express-session");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
 const spoonacularRouter = require("./routes/spoonacular_router");
 const apiRouter = require("./routes/api_router");
+const authRouter = require("./routes/auth_router");
 const errorHandler = require("./middlewares/error_handler");
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false },
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.OAUTH_CLIENT_ID,
-      clientSecret: process.env.OAUTH_SECRET,
-      callbackURL: "http://localhost:8080/auth/google/callback",
-      scope: ["profile", "email"],
-    },
-    function (accessToken, refreshToken, profile, done) {
-      const user = {
-        id: profile.id,
-        displayName: profile.displayName,
-        email: profile.emails[0].value,
-      };
-      done(null, user);
-    }
-  )
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/" }),
-  (req, res) => {
-    const token = jwt.sign(
-      { id: req.user.id, name: req.user.displayName, email: req.user.email },
-      process.env.SESSION_SECRET,
-      { expiresIn: "1h" }
-    );
-    res.redirect(`http://localhost:5173/?token=${token}`);
-  }
-);
-
-app.get("/logout", (req, res) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("http://localhost:5173");
-  });
-});
 
 app.use(express.json());
 
+app.use(authRouter);
 app.use(spoonacularRouter);
 app.use(apiRouter);
 
