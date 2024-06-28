db2b2f3402d45479e429a18614c24487732aa51b
bernadinedao <bernadine.dao@gmail.com>
2024-06-28 22:17:37 +1000

    add logs

--- db/index.js
+++ db/index.js
@@ -4,10 +4,20 @@ const pg = require("pg");
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: true,
   },
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 30000,
+});
+
+pool.on("connect", (client) => {
+  console.log("Connected to the database");
+  client.query("SET statement_timeout = 30000");
+});
+
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
 });
 
 module.exports = pool;
