b1f3f8b0bae74cbb6b9a1b01cdf46308eefa8ad9
bernadinedao <bernadine.dao@gmail.com>
2024-06-28 12:16:23 +1000

    update

--- db/index.js
+++ db/index.js
@@ -6,6 +6,12 @@ const pool = new pg.Pool({
   ssl: {
     rejectUnauthorized: false,
   },
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 30000,
+});
+
+pool.on("connect", (client) => {
+  client.query(`SET statement_timeout = 30000`);
 });
 
 module.exports = pool;
